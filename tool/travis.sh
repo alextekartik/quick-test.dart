083d432dcd9390589364e37c8efb68fd88744f96
Alexandre Roux Tekartik <alex@tekartik.com>
2016-10-08 09:35:04 +0200

    feat: simple test

---
+++ tool/travis.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Fast fail the script on failures.
+# set -e
+
+pkgtest ()
+{
+    echo "\$ pkgtest -sgit $*" 1>&2;
+    eval "pub run pubtest:pubtestpackage -sgit $* -r expanded"
+}
+
+export pkgtest="pub run pubtest:pubtestpackage -sgit"
+pkgtest git://github.com/tekartik/tekartik_browser_utils.dart -p chrome,firefox test/js_utils_browser_test.dart
+pkgtest git://github.com/tekartik/tekartik_io_utils.dart -p vm
+pkgtest git://github.com/tekartik/tekartik_common_utils.dart -p chrome,firefox,vm test/json_utils_test.dart
+pkgtest git://github.com/tekartik/sembast.dart -p vm test/io_factory_test_.dart --name crud
+pkgtest git://github.com/tekartik/idb_shim.dart -p chrome test/idb_browser_test.dart --name index_cursor
+pkgtest git://github.com/tekartik/tekartik_sc.dart -p vm test/sc_test.dart
